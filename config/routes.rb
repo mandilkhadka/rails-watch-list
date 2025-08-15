6f007001e8f735a606bce5c2d3d8d82bbca5d67a
Mandil Khadka <khadkamandil62@gmail.com>
2025-08-15 12:07:08 +0900

    root index added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :lists, only: [:index, :new, :create, :show] do
+  root to: "lists#index"
+  resources :lists, only: [:new, :create, :show] do
     resources :bookmarks, only: [:new, :create]
   end
   resources :bookmarks, only: [:destroy]
